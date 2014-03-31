ccdf1b4c960eb9cda4b39d49f7dbba1572936b6d
Andrea Paterno <illequivosomnesservabit@gmail.com>
2014-03-31 16:32:34 +0200

    P4 adder, first implementation

---
+++ adder/carrygenerator/tree_pg.vhd
@@ -0,0 +1,18 @@
+library ieee; 
+use ieee.std_logic_1164.all; 
+use ieee.std_logic_unsigned.all;
+use work.constants.all;
+
+entity TREE_PG is
+	port(
+		PG0:	in	std_logic_vector(1 downto 0);
+		PG1:	in	std_logic_vector(1 downto 0);
+		PGO:	out	std_logic_vector(1 downto 0);
+	);
+end TREE_PG;
+
+architecture BEHAVIORAL of TREE_G is
+	begin
+		PGO(0)	<= PG0(0) and PG1(0);
+		PGO(1)	<= PG0(1) or ( PG0(0) and PG1(1) );
+end BEHAVIORAL;
